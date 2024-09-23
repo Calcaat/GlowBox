1baa2607fddc3ad887feeb60485d9785d82e7903
Calcaat <joaquincalcagnini@gmail.com>
2024-09-23 20:34:53 -0300

    Add files via upload

---
+++ Pagina/script.js
@@ -0,0 +1,6 @@
+// Función para simular la compra de un producto
+function comprar(producto, precio) {
+    alert("Has comprado: " + producto + " por $" + precio);
+}
+
+// Aquí también podrías añadir la lógica para redirigir al usuario a una plataforma de pago o backend.
