0040d60fae842df1d3bbef4bb1699ceb279bc85c
Calcaat <joaquincalcagnini@gmail.com>
2024-09-23 20:46:36 -0300

    Delete Pagina directory

--- Pagina/script.js
+++
@@ -1,6 +0,0 @@
-// Función para simular la compra de un producto
-function comprar(producto, precio) {
-    alert("Has comprado: " + producto + " por $" + precio);
-}
-
-// Aquí también podrías añadir la lógica para redirigir al usuario a una plataforma de pago o backend.
